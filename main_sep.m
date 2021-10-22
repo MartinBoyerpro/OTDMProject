d1eb91e8f2480de733053cacfe19eb56328d6bb2
Yannlaf <59020809+Yannlaf@users.noreply.github.com>
2021-10-22 14:32:25 +0200

    Update main_sep.m

--- main_sep.m
+++ main_sep.m
@@ -2,12 +2,12 @@ clear;
 %
 % Parameters for dataset generation
 %
-num_target =[8];
+num_target =[1 3 5 7];
 tr_freq    = .5;        
 tr_p       = 250;       
 te_q       = 250;       
-tr_seed    = 590152;    
-te_seed    = 590100;    
+tr_seed    = 123456;    
+te_seed    = 789101;    
 %
 % Parameters for optimization
 %
@@ -41,7 +41,8 @@ function [Xtr,ytr,w0,fo,tr_acc,Xte,yte,te_acc,niter,tex]=uo_nn_solve(num_target,
 %     L2=@(w) L(w,Xtr(:,end),ytr(:,end));
 %     gL2=@(w)gL(w,Xtr(:,end),ytr(:,end));
     
-    w0=ones(1,35)'*0;
+  w0=ones(1,35)'*0;
+  % w0= rand(35,1);
 % uo_nn_Xyplot(Xtr,ytr,w0)
 %     sg = sig(Xtr);
 %     disp("sig : "+size(sg));
@@ -53,7 +54,6 @@ function [Xtr,ytr,w0,fo,tr_acc,Xte,yte,te_acc,niter,tex]=uo_nn_solve(num_target,
 %     disp(size(ytr));
 %     
     %Gradient method
-    
      sizeOfData = size(Xtr);
      %boucle qui va passer chaque colonne des dataset Xtr et ytr dans la
      %fonction Loss, chaque colonne = un chiffre qui va nous permettre
@@ -66,12 +66,12 @@ function [Xtr,ytr,w0,fo,tr_acc,Xte,yte,te_acc,niter,tex]=uo_nn_solve(num_target,
        d=-gL2(w0);
        k=0;
        while(norm(gL2(w0)))>epsG && k<kmax
-            alsave=ialmax;
-            dsave=d;
-            gsave=gL2(w0);
+%             alsave=ialmax;
+%             dsave=d;
+%             gsave=gL2(w0);
             d=-gL2(w0);
             [ialmax,iWout] = uo_BLSNW32(L2,gL2,w0,d,ialmax,c1,c2,kmaxBLS,epsG);
-            ialmax=alsave*((gsave'*dsave)/(gL2(w0)'*d));
+%             ialmax=alsave*((gsave'*dsave)/(gL2(w0)'*d));
             w0 = w0 + ialmax.*d;
             k = k+1; 
 %             disp("k = "+k);
@@ -81,24 +81,26 @@ function [Xtr,ytr,w0,fo,tr_acc,Xte,yte,te_acc,niter,tex]=uo_nn_solve(num_target,
        end
      end
      disp("w0 = "+w0);
-     % calcul de l'accuracy sur le dataset de training cf diapo n°6
-     sumTr = 0;
-     for j=1:sizeOfData(2)
-            if round(y(Xtr(:,j),w0))== ytr(:,j)
-                sumTr = sumTr +1;
-            end
-     end
-     accuracyTr = (100/size (ytr,2 ))*sumTr;
-     disp("accuracy training : "+accuracyTr);
-     % calcul de l'accuracy sur le dataset de test cf diapo n°7
-      sumTe = 0;
-     for h=1:sizeOfData(2)
-            if round(y(Xte(:,h),w0))== yte(:,h)
-                sumTe = sumTe +1;
-            end
-     end
-     accuracyTe = (100/size (yte,2 ))*sumTe;
-     disp("accuracy testing : "+accuracyTe);
+%      % calcul de l'accuracy sur le dataset de training cf diapo n°6
+      uo_nn_Xyplot(Xtr,ytr,w0);
+      uo_nn_Xyplot(Xte,yte,w0);
+%      sumTr = 0;
+%      for j=1:sizeOfData(2)
+%             if round(y(Xtr(:,j),w0))== ytr(:,j)
+%                 sumTr = sumTr +1;
+%             end
+%      end
+%      accuracyTr = (100/size (ytr,2 ))*sumTr;
+%      disp("accuracy training : "+accuracyTr);
+%      % calcul de l'accuracy sur le dataset de test cf diapo n°7
+%       sumTe = 0;
+%      for h=1:sizeOfData(2)
+%             if round(y(Xte(:,h),w0))== yte(:,h)
+%                 sumTe = sumTe +1;
+%             end
+%      end
+%      accuracyTe = (100/size (yte,2 ))*sumTe;
+%      disp("accuracy testing : "+accuracyTe);
        
 end
 
